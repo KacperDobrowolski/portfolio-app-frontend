8be31a7239cc5386edff21e31eb5c2d3edb77a72
KacperDobrowolski <kacper.dobrowolski1234@gmail.com>
2021-03-18 18:37:38 +0100

    Fetch projects list from api in Portfolio component

--- components/Portfolio/Portfolio.js
+++ components/Portfolio/Portfolio.js
@@ -1,8 +1,19 @@
 import styles from './Portfolio.module.scss';
+import React, { useState, useEffect } from 'react';
 import Button from '../Button/Button';
 import Project from '../Project/Project';
+import axios from "axios";
 
 const Portfolio = () => {
+    const [ projects, setProjects ] = useState();
+
+    useEffect( async () => {
+        const res = await axios("http://localhost:8080/api/projects");
+        setProjects(res.data);
+    }, []);
+    if(projects == undefined) {
+        return <div>Loading ...</div>
+    }
     return (
         <section className={styles.portfolio}>
             <h2 className={styles.portfolio_headline}>Moje projekty</h2>
@@ -14,15 +25,11 @@ const Portfolio = () => {
                 </ul>
             </div>
             <div className={styles.projects_list}>
-                <div className={styles.list_item}>
-                    <Project />
-                </div>
-                <div className={styles.list_item}>
-                    <Project />
-                </div>
-                <div className={styles.list_item}>
-                    <Project />
-                </div>
+                { projects.data.map(project => {
+                    return <div className={styles.list_item} key={project._id}>
+                        <Project name={project.name} url={project.url} image={"/assets/images/header.jpg"} />
+                    </div>
+                }) }
             </div>
             <Button text="Zobacz więcej" color={false}/>
         </section>
