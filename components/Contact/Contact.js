3c311461489ccbd4347b6f487df7a88b2ff92ea1
KacperDobrowolski <kacper.dobrowolski1234@gmail.com>
2021-03-18 19:31:18 +0100

    Contact Form / Added values from inputs to state when typing

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -1,21 +1,46 @@
 import styles from './Contact.module.scss';
+import React, { useState } from 'react';
 
 const Contact = () => {
+    const [ name, setName ] = useState("");
+    const [ email, setEmail ] = useState("");
+    const [ msg, setMsg ] = useState("");
+
+    const input = (e) => {
+        const target = e.target;
+        switch(target.name) {
+            case 'name':
+                setName(target.value);
+                break;
+            case 'email':
+                setEmail(target.value);
+                break;
+            case 'msg':
+                setMsg(target.value);
+                break;
+        }
+    }
+
+    const submit = (e) => {
+        e.preventDefault();
+
+    }
+
     return (
         <section className={styles.contact} id="contact">
             <h2 className={styles.contact_headline}>Skontaktuj się ze mną</h2>
             <form className={styles.contact_form}>
                 <div className={styles.input_group}>
                     <label htmlFor="name" className={styles.contact_label}>Imię</label>
-                    <input type="text" id="name" name="name" placeholder="Wpisz swoje imię"/>
+                    <input onChange={input} type="text" id="name" name="name" placeholder="Wpisz swoje imię"/>
         
                     <label htmlFor="email" className={styles.contact_label}>Email</label>
-                    <input type="email" id="email" name="email" placeholder="Wpisz swój adres e-mail"/>
+                    <input onChange={input} type="email" id="email" name="email" placeholder="Wpisz swój adres e-mail"/>
 
                     <label htmlFor="msg" className={styles.contact_label}>Wiadomość</label>
-                    <textarea id="msg" placeholder="Wpisz wiadomość"></textarea> <br /> <br />
+                    <textarea onChange={input} id="msg" name="msg" placeholder="Wpisz wiadomość"></textarea> <br /> <br />
 
-                    <input type="submit" value="Wyślij wiadomość" />
+                    <input onClick={submit} type="submit" value="Wyślij wiadomość" />
                 </div>
             </form>
         </section>
